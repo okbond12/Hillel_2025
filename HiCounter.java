e7efe5e76d2dcd8974b509bba9b9d4c3d82f7b57
Oksana <bondar.ok12@gmail.com>
2025-03-21 16:22:42 +0200

    add_hi_counter

---
+++ HiCounter.java
@@ -0,0 +1,19 @@
+public class HiCounter {
+    public static int countHi(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("hi")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countHi("abc hi ho")); // 1
+        System.out.println(countHi("ABChi hi"));  // 2
+        System.out.println(countHi("hihi"));      // 2
+    }
+}
